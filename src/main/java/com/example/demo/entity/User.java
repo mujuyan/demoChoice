103a73bf0563a6540eef8281a57bde993ed11d0e
mujuyan <17862983037@139.com>
2018-11-01 16:39:28 +0800

    添加注释

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -3,12 +3,14 @@ package com.example.demo.entity;
 import lombok.Data;
 
 /**
- * @classDesc:
+ * @classDesc:用户类
  * @Author: mujuyan
  * @createTime: 2018/11/1 13:25
  */
 @Data
 public class User {
+    //用户名
     private String userName;
+    //密码
     private String passWord;
 }
